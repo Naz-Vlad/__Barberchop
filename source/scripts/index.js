1415de4972116b8b1080f8fe355067547cc95e78
Владимир <10nva63@gmail.com>
2025-02-27 08:40:27 +0300

    разбор скрипта по открытию и закрытию мобильного меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,15 +1,18 @@
 /* в этот файл добавляет скрипты*/
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+const navMain = document.querySelector('.main-nav'); /*находим навигацию -.main-nav */
+const navToggle = document.querySelector('.main-nav__toggle');/* нах. кнопку включения меню*/
+
+navMain.classList.remove('main-nav--nojs');/*удаляем класс при выключенном JS */
+
+/*на кнопку включения меню вешаем обработчик событий при клике на нее */
+navToggle.addEventListener('click', () => { // если при клике на кнпку
+  if (navMain.classList.contains('main-nav--closed')) { // в навигации есть этот класс
+    navMain.classList.remove('main-nav--closed'); // то удаляем этот класс (открываем меню)
+    navMain.classList.add('main-nav--opened'); // а в место ненего дабавляем другой класс
+  } else { // если нет данного класса
+    navMain.classList.add('main-nav--closed'); // добавляем его (закрываем меню)
+    navMain.classList.remove('main-nav--opened'); // и удаляем кдасс main-nav--opened
   }
 });
